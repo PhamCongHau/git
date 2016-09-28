97ded09b40cab297cc234fe2ffc22d6800184fb6
kucute <kid2357@gmail.com>
2016-09-28 16:10:36 +0700

    commit abc

---
+++ TestGit/src/DemoGit/Main.java
@@ -0,0 +1,17 @@
+package DemoGit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data=new SalesData();
+		displaygreeting();
+		data.display();
+		// TODO Auto-generated method stub
+
+	}
+	public static void displaygreeting(){
+		System.out.println("Hello");
+		System.out.println("Xin Chao");
+	}
+
+}
---
+++ TestGit/src/DemoGit/SalesData.java
@@ -0,0 +1,9 @@
+package DemoGit;
+
+public class SalesData {
+	int data[]= {0,3,32};
+	public void display(){
+		System.out.println("Todo: Something");
+	}
+
+}
